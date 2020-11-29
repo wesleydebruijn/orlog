21fefc36409bfc6868bafb7f1286c66d30195e91
jvhoven <contact@jvhoven.nl>
2020-11-29 22:29:56 +0100

    feat: in-progress Mimirs Wisdom

--- apps/game/lib/favor/favors/mimirs_wisdom.ex
+++ apps/game/lib/favor/favors/mimirs_wisdom.ex
@@ -1,6 +1,12 @@
 defmodule Favor.MimirsWisdom do
   @behaviour Favor
+  @moduledoc """
+   Mimirs Wisdom gives you God Favor per damage dealth to you this round.
 
+  Tier 1: you gain 1 God Favor per damage for 3 God Favor.
+  Tier 2: you gain 2 God Favor per damage for 5 God Favor.
+  Tier 3: you gain 3 God Favor per damage for 7 God Favor.
+  """
   @tiers %{
     1 => %{cost: 4, damage: 2},
     2 => %{cost: 8, damage: 5},
