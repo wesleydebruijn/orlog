ce93e131cf68e75e63bc1dd617cf8be43f359d09
Jeffrey van Hoven <contact@jvhoven.nl>
2020-12-06 18:16:30 +0100

    feat: show player tokens (#29)

--- src/web-client/src/components/pages/Game/components/Player/Player.tsx
+++ src/web-client/src/components/pages/Game/components/Player/Player.tsx
@@ -4,18 +4,19 @@ import type { Player as PlayerProps } from '../../../../../types/types'
 
 import './Player.scss'
 
-type Props = Pick<PlayerProps, 'health'> & {
+type Props = Pick<PlayerProps, 'health' | 'tokens'> & {
   name: string
   avatar: string
 }
 
-export default function Player({ name, avatar, health }: Props) {
+export default function Player({ name, avatar, health, tokens }: Props) {
   return (
     <div className="player">
       <img className="player__avatar" alt="" src={avatar}></img>
       <div className="player__name">
         <span>{name}</span>
       </div>
+      <div className="player__tokens">{tokens}</div>
       <div className="player__health">{health}</div>
     </div>
   )
