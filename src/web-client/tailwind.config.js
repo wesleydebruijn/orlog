8fa26bb17649d30e2e082325e5364af1db9e7808
Jeffrey van Hoven <contact@jvhoven.nl>
2020-12-10 18:38:03 +0100

    chore: try tailwind

---
+++ src/web-client/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+
+    },
+    extend: {
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
