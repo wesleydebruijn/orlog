6679071607143094f6e9d965f1810797638d6702
Wesley de Bruijn <wesley.dbr@live.nl>
2020-12-10 22:23:40 +0100

    feat: working sort animation

--- src/web-client/src/components/pages/Game/components/Dice/DiceGrid/DiceGrid.tsx
+++ src/web-client/src/components/pages/Game/components/Dice/DiceGrid/DiceGrid.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useTransition, animated, interpolate } from 'react-spring'
+import shuffle from 'lodash/shuffle'
+
 import { Dice as DiceProps } from '../../../../../../types/types'
 import Dice from '../Dice'
 
@@ -13,11 +16,48 @@ type Props = {
 }
 
 export default function DiceGrid({ dices, rolled, onToggleDice }: Props) {
+  const extendedDices = Object.entries(dices).map(([index, dice]) => ({ ...dice, index }))
+  const [animateDices, setAnimatedDices] = useState(extendedDices)
+
+  const diceWidth = 60
+  const diceMargin = 15
+
+  let width = 0
+  const transitions = useTransition(
+    animateDices.map(dice => ({ ...dice, x: (width += diceWidth) - diceWidth })),
+    dice => dice.index,
+    {
+      from: { width: 0, opacity: 0 },
+      leave: { width: 0, opacity: 0 },
+      enter: ({ x }) => ({ x, width: diceWidth, opacity: 1 }),
+      update: ({ x }) => ({ x, width: diceWidth })
+    }
+  )
+
+  useEffect(() => {
+    setAnimatedDices(extendedDices)
+  }, [dices])
+
   return (
-    <div className="dice-grid">
-      {Object.entries(dices).map(([index, dice]) => (
-        <Dice key={index} {...dice} index={index} rolled={rolled} onClick={onToggleDice} />
-      ))}
-    </div>
+    <>
+      <button onClick={() => setAnimatedDices(shuffle)}>Test</button>
+      <div className="dice-grid">
+        {
+          // @ts-ignore
+          transitions.map(({ item, props: { x, ...rest }, key }, index) => (
+            <animated.div
+              key={key}
+              style={{
+                zIndex: animateDices.length - index,
+                transform: interpolate([x], x => `translate3d(${x}px,0,0)`),
+                ...rest
+              }}
+            >
+              <Dice key={item.index} {...item} rolled={rolled} onClick={onToggleDice} />
+            </animated.div>
+          ))
+        }
+      </div>
+    </>
   )
 }
