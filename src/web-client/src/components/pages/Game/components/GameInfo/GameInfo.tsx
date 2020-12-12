9d5b949c4297e847b87bfebc47f814f2f63b284d
Wesley de Bruijn <wesley.dbr@live.nl>
2020-12-12 22:18:33 +0100

    feat: phase transition animation

--- src/web-client/src/components/pages/Game/components/GameInfo/GameInfo.tsx
+++ src/web-client/src/components/pages/Game/components/GameInfo/GameInfo.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+import { animated, useSpring } from 'react-spring'
 import { useGame } from '../../../../../hooks/useGame'
 import { usePlayer } from '../../../../../hooks/usePlayer'
 
@@ -6,17 +8,32 @@ import './GameInfo.scss'
 export default function GameInfo() {
   const { phase, round } = useGame()
   const { current } = usePlayer()
+  const [transitionPhase, setTransitionPhase] = useState(false)
+
+  const props = useSpring({
+    to: { opacity: transitionPhase ? 1 : 0 },
+    from: { opacity: 0 },
+    config: { duration: 700 },
+    onRest: () => setTransitionPhase(false)
+  })
+
+  useEffect(() => setTransitionPhase(true), [phase.id])
 
   return (
-    <section className="game-info">
-      <div className="game-info__phase">{phase.name} phase</div>
-      <div className="game-info__info">
-        <span className="game-info__info__turn">
-          turns {phase.turns - phase.auto_turns - current.turns + 1}/
-          {phase.turns - phase.auto_turns}
-        </span>
-        <span className="game-info__info__round">round {round}</span>
-      </div>
-    </section>
+    <>
+      <animated.div style={props} className="game-overlay">
+        {phase.name} phase
+      </animated.div>
+      <section className="game-info">
+        <div className="game-info__phase">{phase.name} phase</div>
+        <div className="game-info__info">
+          <span className="game-info__info__turn">
+            turns {phase.turns - phase.auto_turns - current.turns + 1}/
+            {phase.turns - phase.auto_turns}
+          </span>
+          <span className="game-info__info__round">round {round}</span>
+        </div>
+      </section>
+    </>
   )
 }
