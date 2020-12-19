6ea425b47c6a6db8df536bf0868803ca91c304d9
Wesley de Bruijn <wesley.dbr@live.nl>
2020-12-19 22:46:02 +0100

    feat: dice transitions hook

--- src/web-client/src/components/pages/Game/components/Dice/DiceGrid/DiceGrid.tsx
+++ src/web-client/src/components/pages/Game/components/Dice/DiceGrid/DiceGrid.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useTransition, animated, interpolate } from 'react-spring'
+import { animated } from 'react-spring'
 
 import { usePlayer } from '../../../../../../hooks/usePlayer'
 import { useGame } from '../../../../../../hooks/useGame'
@@ -8,28 +8,10 @@ import Dice from '../Dice'
 
 import './DiceGrid.scss'
 import { PhaseId } from '../../../../../../types/types'
+import { useDiceTransitions } from '../../../../../../hooks/useDiceTransitions'
 
 type Props = {
   onToggleDice?: (index: number) => void
-}
-
-const DICE_WIDTH = 85
-const DICE_SPACE = 15
-
-function calculateWidth(dices: any, index: number, faceOff: boolean) {
-  if (!faceOff) return DICE_WIDTH
-
-  const dice = dices[index]
-  const nextDice = dices[index + 1]
-
-  if (
-    !nextDice ||
-    (dice.face.stance === nextDice.face.stance && dice.face.type === nextDice.face.type)
-  ) {
-    return DICE_WIDTH - DICE_SPACE
-  } else {
-    return DICE_WIDTH
-  }
 }
 
 export default function DiceGrid({ onToggleDice }: Props) {
@@ -39,21 +21,7 @@ export default function DiceGrid({ onToggleDice }: Props) {
   const [dices, setDices] = useState(initialDices(player))
   const faceOff = phase.id > PhaseId.Roll
 
-  let width = 0
-  const transitions = useTransition(
-    dices.map((dice, index) => {
-      const diceWidth = calculateWidth(dices, index, faceOff)
-
-      return { ...dice, x: (width += diceWidth) - diceWidth }
-    }),
-    dice => dice.index,
-    {
-      from: { width: 0, opacity: 0 },
-      leave: { width: 0, opacity: 0 },
-      enter: ({ x }) => ({ x, width: DICE_WIDTH, opacity: 1 }),
-      update: ({ x }) => ({ x, width: DICE_WIDTH })
-    }
-  )
+  const { transitions, containerStyle, diceStyle } = useDiceTransitions(dices, faceOff)
 
   useEffect(() => {
     if (faceOff) {
@@ -66,22 +34,12 @@ export default function DiceGrid({ onToggleDice }: Props) {
   return (
     <>
       <div className="dice-grid">
-        <div className="dice-grid__container" style={{ width }}>
-          {
-            // @ts-ignore
-            transitions.map(({ item, props: { x, ...rest }, key }, index) => (
-              <animated.div
-                key={key}
-                style={{
-                  zIndex: dices.length - index,
-                  transform: interpolate([x], x => `translate3d(${x}px,0,0)`),
-                  ...rest
-                }}
-              >
-                <Dice key={item.index} {...item} rolled={player.rolled} onClick={onToggleDice} />
-              </animated.div>
-            ))
-          }
+        <div className="dice-grid__container" style={containerStyle}>
+          {transitions.map(({ item, props, key }, index) => (
+            <animated.div key={key} style={diceStyle(props, index)}>
+              <Dice key={item.index} {...item} rolled={player.rolled} onClick={onToggleDice} />
+            </animated.div>
+          ))}
         </div>
       </div>
     </>
