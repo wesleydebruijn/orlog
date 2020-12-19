af7305c086249927af50d1ee91bee08f59eebc8e
Wesley de Bruijn <wesley.dbr@live.nl>
2020-12-19 23:45:07 +0100

    fix: background css

--- src/web-client/src/components/pages/Game/components/GameBoard/GameBoard.tsx
+++ src/web-client/src/components/pages/Game/components/GameBoard/GameBoard.tsx
@@ -3,27 +3,29 @@ import React from 'react'
 import { useGame } from '../../../../../hooks/useGame'
 
 import ContinueButton from '../ContinueButton/ContinueButton'
-import GameInfo from '../GameInfo/GameInfo'
 import PlayerArea from '../PlayerArea/PlayerArea'
 
 import './GameBoard.scss'
 import { PlayerProvider } from '../../../../../providers/PlayerProvider'
+import Topbar from '../../../../shared/Topbar/Topbar'
 
 export default function GameBoard() {
-  const { player, opponent, actions } = useGame()
+  const { player, opponent, phase, actions } = useGame()
   const { doContinue, selectFavor, toggleDice } = actions
 
   return (
     <section className="game-board">
-      <GameInfo />
+      <Topbar variant="game" title={`${phase.name} phase`} />
       <section className="game-board__field">
-        <PlayerProvider player={opponent} opponent={player}>
-          <PlayerArea />
-        </PlayerProvider>
-        <PlayerProvider player={player} opponent={opponent}>
-          <PlayerArea onSelectFavor={selectFavor} onToggleDice={toggleDice} />
-        </PlayerProvider>
-        <ContinueButton onClick={() => doContinue()} />
+        <div className="game-board__field-bg">
+          <PlayerProvider player={opponent} opponent={player}>
+            <PlayerArea />
+          </PlayerProvider>
+          <PlayerProvider player={player} opponent={opponent}>
+            <PlayerArea onSelectFavor={selectFavor} onToggleDice={toggleDice} />
+          </PlayerProvider>
+          <ContinueButton onClick={() => doContinue()} />
+        </div>
       </section>
     </section>
   )
