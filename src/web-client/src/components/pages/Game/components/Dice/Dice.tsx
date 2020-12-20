a2415e32043f840f7931df11352bf6d28705696b
Wesley de Bruijn <wesley.dbr@live.nl>
2020-12-20 13:01:48 +0100

    remove old assets

--- src/web-client/src/components/pages/Game/components/Dice/Dice.tsx
+++ src/web-client/src/components/pages/Game/components/Dice/Dice.tsx
@@ -1,12 +1,5 @@
-// import { Dice as DiceProps } from '../../../../../types/types'
-
 import './Dice.scss'
 
-// import meleeAttack from './assets/melee-attack.svg'
-// import meleeBlock from './assets/melee-block.svg'
-// import rangedAttack from './assets/ranged-attack.svg'
-// import rangedBlock from './assets/ranged-block.svg'
-// import tokenSteal from './assets/token-steal.svg'
 import classNames from 'classnames'
 import { randomDiceType } from '../../../../../utils/dice'
 import { DiceType } from '../../../../../types/types'
@@ -17,57 +10,6 @@ import {
   RangedBlockIcon,
   TokenStealIcon
 } from '../../../../shared/Icons'
-
-// const FACES: { [index: string]: any } = {
-//   'melee-attack': meleeAttack,
-//   'melee-block': meleeBlock,
-//   'ranged-attack': rangedAttack,
-//   'ranged-block': rangedBlock,
-//   'token-steal': tokenSteal
-// }
-
-// const randomFace = () => Object.keys(FACES)[Math.floor(Math.random() * Object.keys(FACES).length)]
-// const randomFaceClasses = () =>
-//   classnames('face', { 'face--tokens': Math.round(Math.random()) > 0 })
-
-// export default function Dice({
-//   index,
-//   face,
-//   tokens,
-//   keep,
-//   locked,
-//   rolled,
-//   placeholder,
-//   onClick
-// }: Props) {
-//   const classes = classnames('dice', {
-//     'dice--toggleable': onClick !== undefined && !locked,
-//     'dice--placeholder': placeholder
-//   })
-
-//   return (
-//     <div className={classes} onClick={() => onClick && onClick(index)}>
-//       <div className="front">
-//         <img src={FACES[`${face.type}-${face.stance}`]} className="face" alt="" />
-//       </div>
-//       <div className="back">
-//         <img src={FACES[randomFace()]} className={randomFaceClasses()} alt="" />
-//       </div>
-//       <div className="right">
-//         <img src={FACES[randomFace()]} className={randomFaceClasses()} alt="" />
-//       </div>
-//       <div className="left">
-//         <img src={FACES[randomFace()]} className={randomFaceClasses()} alt="" />
-//       </div>
-//       <div className="top">
-//         <img src={FACES[randomFace()]} className={randomFaceClasses()} alt="" />
-//       </div>
-//       <div className="bottom">
-//         <img src={FACES[randomFace()]} className={randomFaceClasses()} alt="" />
-//       </div>
-//     </div>
-//   )
-// }
 
 type Props = {
   value?: DiceType
